0e1202231a327311a205e3278ff0fdd664e9866f
muszkin <muszkin@gmail.com>
2018-03-18 20:59:47 +0100

    revert change in server

--- chat-server/src/main/java/com/chat/server/WebSocketConfig.java
+++ chat-server/src/main/java/com/chat/server/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/register").setAllowedOrigins("*");
+    registry.addEndpoint("/register").setAllowedOrigins("*").withSockJS();
   }
 
   @Override
