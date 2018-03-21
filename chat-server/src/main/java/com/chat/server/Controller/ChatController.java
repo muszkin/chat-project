0bbe0cc6fcd19f33ef40489faed8bd85ecbc02a3
Karol <swieca.karol@gmail.com>
2018-03-21 20:15:22 +0100

    yandex translator

--- chat-server/src/main/java/com/chat/server/Controller/ChatController.java
+++ chat-server/src/main/java/com/chat/server/Controller/ChatController.java
@@ -11,24 +11,26 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
-  @Autowired
-  private SimpMessageSendingOperations messageSendingOperations;
+    @Autowired
+    private SimpMessageSendingOperations messageSendingOperations;
 
-  @MessageMapping("/hello")
-  public void hello(HelloMessage message, SimpMessageHeaderAccessor headerAccessor) throws Exception {
-    Thread.sleep(100); // simulated delay
-    messageSendingOperations.convertAndSend("/topic/private/" + headerAccessor.getSessionId(), new ChatMessage("Hello, " + message.getName() + "!"));
-  }
+    @MessageMapping("/hello")
+    public void hello(HelloMessage message, SimpMessageHeaderAccessor headerAccessor) throws Exception {
+        Thread.sleep(100); // simulated delay
+        messageSendingOperations.convertAndSend("/topic/private/" + headerAccessor.getSessionId(), new ChatMessage("Hello, " + message.getName() + "!"));
+    }
 
-  @MessageMapping("/chat")
-  public void chat(ChatMessage message, SimpMessageHeaderAccessor headerAccessor) throws Exception {
-    Thread.sleep(100); // simulated delay
-    
-// auth problem
-//    GoogleTranslateController polishToEnglishTranslator = new GoogleTranslateController("pl","en");
-//    String transaltedByGoogle = polishToEnglishTranslator.traslateString(message.getContent());
-            
-    messageSendingOperations.convertAndSend("/topic/private/" + headerAccessor.getSessionId(), new ChatMessage("<b>You</b>: " + message.getContent() ));
-  }
+    @MessageMapping("/chat")
+    public void chat(ChatMessage message, SimpMessageHeaderAccessor headerAccessor) throws Exception {
+        Thread.sleep(100); // simulated delay
+
+// auth problem - zmienna systemowa jest pusta :C
+//        GoogleTranslateController polishToEnglishTranslator = new GoogleTranslateController("pl", "en");
+//        String transaltedByGoogle = polishToEnglishTranslator.traslateString(message.getContent());
+//        messageSendingOperations.convertAndSend("/topic/private/" + headerAccessor.getSessionId(), new ChatMessage("<b>You</b>: " + message.getContent() + transaltedByGoogle));
+
+        YandexTransalteController translator = new YandexTransalteController("en");
+        messageSendingOperations.convertAndSend("/topic/private/" + headerAccessor.getSessionId(), new ChatMessage("<b>You</b>: " + message.getContent() + translator.traslateString(message.getContent())));
+    }
 
 }
