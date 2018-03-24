6f62fe8e335c942974971a35ac1be856482ac316
yasiumaster <yasiumaster@gmail.com>
2018-03-24 15:14:21 +0100

    Cleanup

--- chat-server/src/main/java/com/chat/server/Controller/AdminController.java
+++ chat-server/src/main/java/com/chat/server/Controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.chat.server.Controller;
 
 import com.chat.server.Message.ChatMessage;
+import com.chat.server.MongoDB.MongoDBClient;
 import com.chat.server.Translation.YandexTransalteClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -12,32 +13,27 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class AdminController {
 
-    @Autowired
-    private ApplicationContext applicationContext;
+  @Autowired
+  private ApplicationContext applicationContext;
+  @Autowired
+  private MongoDBClient mongoDBClient;
+  @Autowired
+  private SimpMessageSendingOperations messageSendingOperations;
 
-    @Autowired
-    private SimpMessageSendingOperations messageSendingOperations;
+  @MessageMapping("/admin")
+  public void sendToUser(ChatMessage message, SimpMessageHeaderAccessor headerAccessor) {
+    //pobierz info z mongo
 
-//  public String sendToAllConnected(@RequestParam("message") String message) {
-//    List<SessionConnectedEvent> sessionConnectedEvents
-//      = (List<SessionConnectedEvent>) applicationContext.getBean("sessionConnectedEvents");
-//    sessionConnectedEvents.forEach(event -> {
-//      String sessionId = (String) event.getMessage().getHeaders().get("simpSessionId");
-//      messageSendingOperations.convertAndSend("/topic/private/" + sessionId, new ChatMessage(message));
-//    });
-//    return "OK";
-//  }
-    @MessageMapping("/admin")
-    public void sendToUser(ChatMessage message, SimpMessageHeaderAccessor headerAccessor) {
-        //pobierz info z mongo
-        
-        String adminSourceLang = "pl";
-        String userTargetLang = "it";
+    String adminSourceLang = "pl";
+    String userTargetLang = "it";
 
-        //tlumaczymy
-        YandexTransalteClient translator = new YandexTransalteClient(userTargetLang, adminSourceLang);
-        message.setTranslatedContend(translator.traslateString(message.getContent()));
-        messageSendingOperations.convertAndSend("/topic/private/" + headerAccessor.getNativeHeader("user-id").get(0), message);
-    }
+    //tlumaczymy
+    YandexTransalteClient translator = new YandexTransalteClient(userTargetLang, adminSourceLang);
+    message.setContent(translator.traslateString(message.getContent()));
+    message.setUserIsAdmin();
+    String userId = headerAccessor.getNativeHeader("user-id").get(0);
+    messageSendingOperations.convertAndSend("/topic/private/" + userId, message);
+    mongoDBClient.addNewChatMessageToUserMessages(userId, message);
+  }
 
 }
