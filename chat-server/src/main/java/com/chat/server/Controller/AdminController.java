c2ad6e2edf36cf7d2a09b3009f0c11a9f3d5d376
yasiumaster <yasiumaster@gmail.com>
2018-03-24 12:35:00 +0100

    Chat-admin

--- chat-server/src/main/java/com/chat/server/Controller/AdminController.java
+++ chat-server/src/main/java/com/chat/server/Controller/AdminController.java
@@ -1,17 +1,14 @@
 package com.chat.server.Controller;
 
 import com.chat.server.Message.ChatMessage;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.stereotype.Controller;
 
-@RestController
-@RequestMapping("/admin")
+@Controller
 public class AdminController {
 
   @Autowired
@@ -20,21 +17,19 @@ public class AdminController {
   @Autowired
   private SimpMessageSendingOperations messageSendingOperations;
 
-  @RequestMapping("/sendToAllConnected")
-  public String sendToAllConnected(@RequestParam("message") String message) {
-    List<SessionConnectedEvent> sessionConnectedEvents
-      = (List<SessionConnectedEvent>) applicationContext.getBean("sessionConnectedEvents");
-    sessionConnectedEvents.forEach(event -> {
-      String sessionId = (String) event.getMessage().getHeaders().get("simpSessionId");
-      messageSendingOperations.convertAndSend("/topic/private/" + sessionId, new ChatMessage(message));
-    });
-    return "OK";
-  }
+//  public String sendToAllConnected(@RequestParam("message") String message) {
+//    List<SessionConnectedEvent> sessionConnectedEvents
+//      = (List<SessionConnectedEvent>) applicationContext.getBean("sessionConnectedEvents");
+//    sessionConnectedEvents.forEach(event -> {
+//      String sessionId = (String) event.getMessage().getHeaders().get("simpSessionId");
+//      messageSendingOperations.convertAndSend("/topic/private/" + sessionId, new ChatMessage(message));
+//    });
+//    return "OK";
+//  }
 
-  @RequestMapping("/sendToUser")
-  public String sendToUser(@RequestParam("id") String sessionId, @RequestParam("message") String message) {
-    messageSendingOperations.convertAndSend("/topic/private/" + sessionId, new ChatMessage(message));
-    return "OK";
+  @MessageMapping("/admin")
+  public void sendToUser(ChatMessage message, SimpMessageHeaderAccessor headerAccessor) {
+    messageSendingOperations.convertAndSend("/topic/private/" + headerAccessor.getNativeHeader("user-id").get(0), message);
   }
 
 }
--- chat-server/src/main/java/com/chat/server/Message/ChatMessage.java
+++ chat-server/src/main/java/com/chat/server/Message/ChatMessage.java
@@ -3,16 +3,24 @@ package com.chat.server.Message;
 public class ChatMessage {
 
   private String content;
+  private String userId;
 
   public ChatMessage() {
   }
 
-  public ChatMessage(String content) {
+  public ChatMessage(String content, String userId) {
     this.content = content;
+    this.userId = userId;
   }
+
 
   public String getContent() {
     return content;
   }
+
+  public String getUserId() {
+    return userId;
+  }
+  
 
 }
